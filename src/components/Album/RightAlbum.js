2462412c9bb12dff147743295909a65c5fa15e50
phuonggavt19 <124556767+phuonggavt19@users.noreply.github.com>
2023-02-26 12:13:10 +0700

    next and prev music

--- src/components/Album/RightAlbum.js
+++ src/components/Album/RightAlbum.js
@@ -1,10 +1,12 @@
+import { forwardRef } from "react";
+import { useSelector } from "react-redux";
 import icons from "../../ultis/icons";
 
 import SongAlbum from "./SongAlbum";
 
-function RightAlbum({ songs, titleRight }) {
-    console.log("title:", songs);
+function RightAlbum({ titleRight }, ref) {
     const { BiTransfer } = icons;
+    const listMusic = useSelector((state) => state.app.listMusic);
 
     return (
         <div className="col-span-3 text-secondary text-sm font-medium h-full overflow-hidden hover:overlay">
@@ -12,7 +14,7 @@ function RightAlbum({ songs, titleRight }) {
                 <span className="whitespace-nowrap mr-1">Lời tựa:</span>
                 <span className="ml-1 text-main">{titleRight}</span>
             </div>
-            <div className="grid grid-cols-none h-3 mt-4">
+            <div className="grid grid-cols-none h-3 mt-4" ref={ref}>
                 <div className=" grid grid-cols-6 px-6 relative border-b py-3 border-alpha group-hover/edit:border-transparent">
                     <BiTransfer className="absolute left-0 bottom-1/2 translate-y-1/2" />
                     <span className="grid col-span-3">Bài hát</span>
@@ -22,19 +24,23 @@ function RightAlbum({ songs, titleRight }) {
                     </span>
                 </div>
 
-                {songs?.map((song, index) => (
-                    <SongAlbum
-                        key={index}
-                        title={song.title}
-                        duration={song.duration}
-                        album={song.album}
-                        artists={song.artists}
-                        img={song.thumbnailM}
-                    />
-                ))}
+                <div ref={ref}>
+                    {listMusic?.map((music, index) => (
+                        <SongAlbum
+                            key={index}
+                            title={music.name}
+                            duration={music.duration}
+                            album={music.album}
+                            artists={music.artists}
+                            img={music.img}
+                            idSong={music.id}
+                            index={index}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
 }
 
-export default RightAlbum;
+export default forwardRef(RightAlbum);
